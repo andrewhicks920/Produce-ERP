2d08ba70387fdeb444335fac97b018c0781ac7ae
Andrew Hicks <andrewhicks920@gmail.com>
2026-03-08 14:51:20 -0700

    Removed Product DTO and Warehouse logic

--- src/main/java/com/andrewhicks/produce_erp/repository/WarehouseRepository.java
+++
@@ -1,7 +0,0 @@
-package com.andrewhicks.produce_erp.repository;
-
-import com.andrewhicks.produce_erp.model.Warehouse;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface WarehouseRepository extends JpaRepository<Warehouse, Long> {
-}
